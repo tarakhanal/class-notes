5c53f5a72b87c6ba0d16e4a3f6f4af418d41e8e1
Tara Khanal <tarakh99@gmail.com>
2022-09-12 13:50:53 -0400

    Remove links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,30 +93,15 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
+                label: 'Instagram',
+                href: 'https://instagram.com/astonishingpixels',
+              }
             ],
           },
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
+
             ],
           },
         ],
